d8ef8607073f25ba431ea0b35ba0cbf230838d3c
heyan <heyan@impcas.ac.cn>
2020-10-26 15:14:21 +0800

    add 01-basic:B-hello-headers

---
+++ B-hello-headers/build/CMakeFiles/hello_headers.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/heyan/work/CMake/01-basic/B-hello-headers/src/Hello.cpp" "/Users/heyan/work/CMake/01-basic/B-hello-headers/build/CMakeFiles/hello_headers.dir/src/Hello.cpp.o"
+  "/Users/heyan/work/CMake/01-basic/B-hello-headers/src/main.cpp" "/Users/heyan/work/CMake/01-basic/B-hello-headers/build/CMakeFiles/hello_headers.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
